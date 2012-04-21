22f8de030b313e0cacd4cdf6bf49f91992190bb0
Lucimar Leandro Bezerra <lucimar@highlevel-it.net>
2012-04-21 11:21:58 -0300

    Add contac page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -43,4 +43,19 @@ describe "Static pages" do
                     :text => "Ruby on Rails Tutorial Sample App | About Us")
     end
   end
+  
+  describe "About page" do
+
+    it "should have the h1 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact')
+    end
+
+    it "should have the title 'About Us'" do
+      visit '/static_pages/contact'
+      page.should have_selector('title',
+                    :text => "Ruby on Rails Tutorial Sample App | Contact")
+    end
+  end
+  
 end
